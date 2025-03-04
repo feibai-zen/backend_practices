c479ac87dfd9470e1f0be1f4c26e7332d4b02340
Liyuanlong <959769993@qq.com>
2025-03-04 12:54:49 +0800

    mv the design patter

--- study_demos/src/main/java/com/feibai/study/demos/designpatterns/adapter/Adaptee.java
+++
@@ -1,7 +0,0 @@
-package com.feibai.study.demos.designpatterns.adapter;
-
-public class Adaptee {
-  public void method1() {
-    System.out.println("method 1");
-  }
-}
--- study_demos/src/main/java/com/feibai/study/demos/designpatterns/adapter/Target.java
+++
@@ -1,6 +0,0 @@
-package com.feibai.study.demos.designpatterns.adapter;
-
-public interface Target {
-  void method1();
-  void method2();
-}
